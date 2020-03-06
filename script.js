0898b04aa1ed5f90e640b538c0ffaa9e2e60d015
sonalsinghvs <49417594+sonalsinghvs@users.noreply.github.com>
2020-03-05 22:12:59 -0700

    Added files

---
+++ script.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    llama: "https://en.wikipedia.org/wiki/Llama",
+    alpaca: "https://en.wikipedia.org/wiki/Alpaca",
+    llamaTitle: "Llamas are fun",
+    alpacaTitle:"Alpacas are amazing",
+    baseStyle:{
+      background: 'grey'
+    },
+    currentRoute: 'home'
+  }
+})
